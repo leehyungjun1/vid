393f9b51a98434de803a12c37f6d29aa444e41f1
james <unpobby@nate.com>
2023-05-12 17:22:31 +0900

    add button

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video/component/custom_icon_button.dart';
+import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
@@ -14,14 +18,87 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeController();
+  }
+
+  initializeController() async {
+    final videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoController.initialize();
+
+    setState(() {
+      this.videoController = videoController;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'CustomVideoPlayer',
-        style: TextStyle(
-          color: Colors.white,
-        ),
+    if (videoController == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoController!,
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Slider(
+              value: videoController!.value.position.inSeconds.toDouble(),
+              onChanged: (double val) {
+                videoController!.seekTo(
+                  Duration(seconds: val.toInt()),
+                );
+              },
+              min: 0,
+              max: videoController!.value.duration.inSeconds.toDouble(),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: CustomIconButton(
+              onPressed: () {},
+              iconData: Icons.photo_camera_back,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomIconButton(
+                  onPressed: () {},
+                  iconData: Icons.rotate_left,
+                ),
+                CustomIconButton(
+                  onPressed: () {},
+                  iconData: videoController!.value.isPlaying
+                      ? Icons.pause
+                      : Icons.play_arrow,
+                ),
+                CustomIconButton(
+                  onPressed: () {},
+                  iconData: Icons.rotate_right,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
